459d714107b14fdc4d8818a61bd7846324535dea
Mike Starr <imhs@protonmail.com>
2025-04-14 13:43:13 -0700

    fix Portfolio text

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -2,9 +2,8 @@ export const Portfolio = () => {
   return (
     <section>
       <p className="w-full text-sm md:w-4/5 xl:-ml-20">
-        Loosely designed in <span className="text-[#ccd6f6]">Figma</span> and coded in{' '}
-        <span className="text-[#ccd6f6]">Cursor</span> by yours truly. Built with{' '}
-        <span className="text-[#ccd6f6]">Vite</span> and{' '}
+        Coded in <span className="text-[#ccd6f6]">Cursor</span> by yours truly. Built with{' '}
+        <span className="text-[#ccd6f6]">React</span> and{' '}
         <span className="text-[#ccd6f6]">Tailwind CSS</span>, deployed with{' '}
         <span className="text-[#ccd6f6]">Vercel</span>. All text is set in the{' '}
         <span className="text-[#ccd6f6]">Inter</span> typeface.{' '}
